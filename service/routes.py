70bf564aa2ff5c1ed6651cbda96edcf5fc0574b0
Adesina Ayodele <adesinaayodele48@gmail.com>
2024-09-24 03:12:31 -0400

    Fixed line lengths and formatting issues in route and log handler files

--- service/routes.py
+++ service/routes.py
@@ -7,7 +7,6 @@ from service.common import status
 
 COUNTER = {}
 
-
 ############################################################
 # Health Endpoint
 ############################################################
@@ -40,7 +39,10 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
+    counters = [
+        dict(name=count[0], counter=count[1]) 
+        for count in COUNTER.items()
+    ]
 
     return jsonify(counters)
 
@@ -54,7 +56,10 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
+        return abort(
+            status.HTTP_409_CONFLICT, 
+            f"Counter {name} already exists"
+        )
 
     COUNTER[name] = 0
 
@@ -95,12 +100,16 @@ def update_counters(name):
         name (str): The name of the counter to update.
 
     Returns:
-        JSON response with the updated counter value if found, or an error if not.
+        JSON response with the updated counter value 
+        if found, or an error if not.
     """
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND, 
+            f"Counter {name} does not exist"
+        )
 
     COUNTER[name] += 1
 
